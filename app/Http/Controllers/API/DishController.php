afcde8570065226e0e181cbadbfc81fada563c0a
Angel Garcia <agarcia@witann.com>
2021-06-23 17:07:25 -0500

    Dish categories id pluck

--- app/Http/Controllers/API/DishController.php
+++ app/Http/Controllers/API/DishController.php
@@ -8,16 +8,18 @@ use App\Http\Controllers\API\BaseController as BaseController;
 use App\Http\Resources\Dish as DishResource;
 
 use App\Models\Dish;
+use App\Models\Category;
 
 class DishController extends BaseController
 {
     public function index(Request $request)
     {
         $queries = $request->query();
+        $categoriesIds = Category::where('restaurant_id', env('RESTAURANT_ID'))->pluck('id');
         if(count($queries) > 0)
-        $dishes = Dish::where($queries)->where('restaurant_id', env('RESTAURANT_ID'))->with(['images'])->get();
+        $dishes = Dish::where($queries)->whereIn('category_id', $categoriesIds)->with(['images'])->get();
         else
-        $dishes = Dish::where('restaurant_id', env('RESTAURANT_ID'))->with(['images'])->get();
+        $dishes = Dish::whereIn('category_id', $categoriesIds)->with(['images'])->get();
 
         return $this->sendResponse(DishResource::collection($dishes), 'Dishes retrieved successfully.');
     }
